d61537f5fdd2b7eab071ef8dab88f9b7e7d0b445
Sujan36 <90554688+Sujan36@users.noreply.github.com>
2023-03-30 19:15:25 +0530

    Add files via upload

---
+++ home/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import Sum
+#Create your models here.
+SELECT_CATEGORY_CHOICES = [
+    ("Food","Food"),
+    ("Travel","Travel"),
+    ("Shopping","Shopping"),
+    ("Necessities","Necessities"),
+    ("Entertainment","Entertainment"),
+    ("Other","Other")
+ ]
+ADD_EXPENSE_CHOICES = [
+     ("Expense","Expense"),
+     ("Income","Income")
+ ]
+PROFESSION_CHOICES =[
+    ("Employee","Employee"),
+    ("Business","Business"),
+    ("Student","Student"),
+    ("Other","Other")
+]
+class Addmoney_info(models.Model):
+    user = models.ForeignKey(User,default = 1, on_delete=models.CASCADE)
+    add_money = models.CharField(max_length = 10 , choices = ADD_EXPENSE_CHOICES )
+    quantity = models.BigIntegerField()
+    Date = models.DateField(default = now)
+    Category = models.CharField( max_length = 20, choices = SELECT_CATEGORY_CHOICES , default ='Food')
+    class Meta:
+        db_table:'addmoney'
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profession = models.CharField(max_length = 10, choices=PROFESSION_CHOICES)
+    Savings = models.IntegerField( null=True, blank=True)
+    income = models.BigIntegerField(null=True, blank=True)
+    image = models.ImageField(upload_to='profile_image',blank=True)
+    def __str__(self):
+       return self.user.username
+
+
+
+#stock
+#from django.db import models
+#from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    responsible = models.ForeignKey(User, on_delete=models.CASCADE)
+    week_number = models.CharField(max_length=2, blank=True)
+    end_date = models.DateField()
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        print(self.start_date.isocalendar()[1])
+        if self.week_number == "":
+            self.week_number = self.start_date.isocalendar()[1]
+        super().save(*args, **kwargs)
+   
+
